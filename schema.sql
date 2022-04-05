9defb901b2d8881ffe76e707bb6e27ee83fb6c8c
Jackie Cho <jackie.s.cho@gmail.com>
2022-04-05 01:03:31 -0400

    add retirement info

--- schema.sql
+++ schema.sql
@@ -36,8 +36,8 @@ CREATE TABLE salaries (
 );
 
 CREATE TABLE dept_emp (
-    dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL, 
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
     FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
@@ -50,8 +50,15 @@ CREATE TABLE titles (
   title VARCHAR(50) NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
+
+DROP TABLE dept_emp CASCADE; -- CASCADE - removes the connection to other tables in database
+
 SELECT * FROM departments;
+SELECT * FROM employees;
+SELECT * FROM salaries;
+SELECT * FROM titles;
+SELECT * FROM dept_manager;
+SELECT * FROM dept_emp;
